8975834ccd9588d00de8034a13c4a76bd0cc45a9
vitypm <dev.vitypm@gmail.com>
2021-07-20 18:07:26 +1000

    require doctrine-orm-admin-bundle, add Entity:Author,books, add Controller Admin/Book.Autor
    Подключаем Sonata doctrine-orm-admin-bundle, для прямой поддержки с работой базы данной.
    composer require sonata-project/doctrine-orm-admin-bundle
    
    Создаем Сущности (Модели, Entity)
    - php bin/console make:Entity
    При выборе type field, если хотим создать сущности-> ManyToMany, OneToMany, OneToOne
     Создаем миграцию и отправляем в базу данных
    - php bin/console make:migration
    - php bin/console doctrine:migrations:migrate
    
     Для того чтобы быстро взаимодействовать с Admin-sonata после создании сущности,
    - php bin/console sonata:admin:list

---
+++ src/Entity/Author.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AuthorRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=AuthorRepository::class)
+ */
+class Author
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $surname;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Book::class, mappedBy="auhors")
+     */
+    private $books;
+
+    public function __construct()
+    {
+        $this->books = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSurname(): ?string
+    {
+        return $this->surname;
+    }
+
+    public function setSurname(string $surname): self
+    {
+        $this->surname = $surname;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Book[]
+     */
+    public function getBooks(): Collection
+    {
+        return $this->books;
+    }
+
+    public function addBook(Book $book): self
+    {
+        if (!$this->books->contains($book)) {
+            $this->books[] = $book;
+            $book->addAuhor($this);
+        }
+
+        return $this;
+    }
+
+    public function removeBook(Book $book): self
+    {
+        if ($this->books->removeElement($book)) {
+            $book->removeAuhor($this);
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/Book.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=BookRepository::class)
+ */
+class Book
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Author::class, inversedBy="books")
+     */
+    private $auhors;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $publication;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $brochureFilename;
+
+    public function __construct()
+    {
+        $this->auhors = new ArrayCollection();
+    }
+
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Author[]
+     */
+    public function getAuhors(): Collection
+    {
+        return $this->auhors;
+    }
+
+    public function addAuhor(Author $auhor): self
+    {
+        if (!$this->auhors->contains($auhor)) {
+            $this->auhors[] = $auhor;
+        }
+
+        return $this;
+    }
+
+    public function removeAuhor(Author $auhor): self
+    {
+        $this->auhors->removeElement($auhor);
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPublication(): ?\DateTimeInterface
+    {
+        return $this->publication;
+    }
+
+    public function setPublication(\DateTimeInterface $publication): self
+    {
+        $this->publication = $publication;
+
+        return $this;
+    }
+
+    public function getBrochureFilename(): ?string
+    {
+        return $this->brochureFilename;
+    }
+
+    public function setBrochureFilename(string $brochureFilename): self
+    {
+        $this->brochureFilename = $brochureFilename;
+
+        return $this;
+    }
+
+
+}
